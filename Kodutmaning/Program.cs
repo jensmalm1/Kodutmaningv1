73c10f51c33bec027cd30daf9a05de9d1c56322c
JensMalm <jensmalm873@gmail.com>
2018-04-29 22:38:56 +0200

    Add project files.

---
+++ Kodutmaning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kodutmaning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
